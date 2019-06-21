4bff8576852415e8c8f4c0627bca23b38c8e4f1e
annettesunday <annette.odhiambo@andela.com>
2019-06-21 10:43:24 +0300

    ch(src): create app layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
+import "./index.css";
 
-function Hi() {
-    debugger;
-  return <p>Hello</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
