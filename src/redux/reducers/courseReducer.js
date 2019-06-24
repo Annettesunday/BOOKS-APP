fc659e58e0ae4c126bf30d76d657813bde60ef5d
annettesunday <annette.odhiambo@andela.com>
2019-06-24 15:16:56 +0300

    ch(courseReducer): handle updates in reducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,5 +1,5 @@
 import * as types from "../actions/actionTypes";
-import initialState from "./initialState"
+import initialState from "./initialState";
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
@@ -7,6 +7,10 @@ export default function courseReducer(state = initialState.courses, action) {
       return [...state, { ...action.course }];
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course
+      );
     default:
       return state;
   }
