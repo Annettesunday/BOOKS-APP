914d8710990fe2a04d3073d4eb64c1e3f9597805
annettesunday <annette.odhiambo@andela.com>
2019-06-24 15:12:58 +0300

    ch(courseActions): add save thunk and action creator

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,7 +3,7 @@ import initialState from "./initialState"
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
