f29766f756f8321338e727547d4d01756ff1f074
annettesunday <annette.odhiambo@andela.com>
2019-06-24 14:42:38 +0300

    ch(ManageCoursePage): call CourseForm component to MCP component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+function ManageCoursePage({
+  courses,
+  authors,
+  loadCourses,
+  loadAuthors,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => {
@@ -19,27 +29,21 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
     }
   }, []);
 
-  return <h2> Manage Course</h2>;
+  return <CourseForm course={course} errors={errors} authors={authors} />;
 }
 
 ManageCoursePage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
-  loadAuthors: PropTypes.func.isRequired
+  loadAuthors: PropTypes.func.isRequired,
+  course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    courses:
-      state.authors.length === 0
-        ? []
-        : state.courses.map(course => {
-            return {
-              ...course,
-              authorName: state.authors.find(a => a.id === course.authorId).name
-            };
-          }),
+    course: newCourse,
+    courses: state.courses,
     authors: state.authors
   };
 }
