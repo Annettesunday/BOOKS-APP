8d4cc33f5d5229bcd853f546f78244318f0939a6
annettesunday <annette.odhiambo@andela.com>
2019-07-03 16:38:31 +0300

    test(test): testing the store

---
+++ src/redux/store.test.js
@@ -0,0 +1,21 @@
+// automated integration test
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import initialState from "./reducers/initialState";
+import * as courseActions from "./actions/courseActions";
+
+it("should habdle creating courses", () => {
+  // arrange
+  const store = createStore(rootReducer, initialState);
+  const course = {
+    title: "Clean"
+  };
+
+  //   act
+  const action = courseActions.createCourseSuccess(course);
+  store.dispatch(action);
+
+  // assert
+  const createdCourse = store.getState().courses[0];
+  expect(createdCourse).toEqual(course);
+});
